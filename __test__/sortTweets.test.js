2f79144e6637f5f507c11449e14fb60549df5fc0
Roylu7384 <roylu7384@gmail.com>
2022-05-06 16:49:10 -0400

    Debug sortTweets function

--- __test__/sortTweets.test.js
+++ __test__/sortTweets.test.js
@@ -6,8 +6,8 @@ describe('Test tweets sorting helper function', () => {
     const tweets = [{ id: 2 }, { id: 1 }, { id: 3 }];
     const sortedtweets = [{ id: 3 }, { id: 2 }, { id: 1 }];
 
-    const tweetsBylikes = [{ likes: 2, id: 1 }, { likes: 1, id: 2 }, { likes: 3, id: 3 }];
-    const descSortedtweets = [{ likes: 3, id: 3 }, { likes: 2, id: 1 }, { likes: 1, id: 2 }];
+    const tweetsBylikes = [{ public_metrics: {likes: 2}, id: 1 }, { public_metrics: {likes: 1}, id: 2 }, { public_metrics: {likes: 3}, id: 3 }];
+    const descSortedtweets = [{ public_metrics: {likes: 3}, id: 3 }, { public_metrics: {likes: 2}, id: 1 }, { public_metrics: {likes: 1}, id: 2 }];
 
     expect(sortTweets(null, null, tweets)).toEqual(sortedtweets);
     expect(sortTweets('id', 'desc', tweets)).toEqual(sortedtweets);
--- helper/sortTweets.js
+++ helper/sortTweets.js
@@ -1,14 +1,22 @@
 const sortTweets = (sortBy, direction, tweets) => {
-
-  // default sorting criteria is base on id in ascending direction
-  const sortField = sortBy ? sortBy : 'id';
+  // default sorting direction is descending
   const sortDirection = direction ? direction : 'desc';
-
-  if (sortDirection === 'desc') {
-    return tweets.sort((tweetA, tweetB) => tweetB[sortField] - tweetA[sortField]);
-  } else {
-    return tweets.sort((tweetA, tweetB) => tweetA[sortField] - tweetB[sortField]);
+  
+  if (sortBy && sortBy !== 'id') {
+    if (sortDirection === 'desc') {
+      return tweets.sort((tweetA, tweetB) => tweetB.public_metrics[sortBy] - tweetA.public_metrics[sortBy]);
+    } else {
+      return tweets.sort((tweetA, tweetB) => tweetA.public_metrics[sortBy] - tweetB.public_metrics[sortBy]);
+    }
   }
+  
+  // default sorting criteria is base on id in descending direction
+  if (sortDirection === 'desc') {
+    return tweets.sort((tweetA, tweetB) => tweetB.id - tweetA.id);
+  } else {
+    return tweets.sort((tweetA, tweetB) => tweetA.id - tweetB.id);
+  }
+  
 };
 
 module.exports = sortTweets;
