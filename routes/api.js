c33ad8f18dea4febc9e23cd96f1bd984cb063e29
Roylu7384 <roylu7384@gmail.com>
2022-05-06 10:23:32 -0400

    Add route for getting tweets and begin with query validation

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// helper funcitons
+const queryValidator = require('../helper/queryValidator');
+
 module.exports = cachedResult => {
-  
   // Route for pinging the server
-  router.get('/ping', (req, res) => {
+  router.get("/ping", (req, res) => {
     res.json({ success: true });
   });
-  
+
+  // Route for listing all tweets according to query param
+  router.get("/tweets", (req, res) => {
+    const { names, sortBy, direction } = req.query;
+    const errMsg = queryValidator(names, sortBy, direction);
+
+    if (errMsg) {
+      res.status(400).json(errMsg);
+      return;
+    }
+  });
+
   return router;
 };
