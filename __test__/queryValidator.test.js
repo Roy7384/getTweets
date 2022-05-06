a9fc169b4543e2dd391f9ef812bf98f43270764f
Roylu7384 <roylu7384@gmail.com>
2022-05-06 10:27:52 -0400

    Setup queryValidator test

---
+++ __test__/queryValidator.test.js
@@ -0,0 +1,21 @@
+const queryValidator = require('../helper/queryValidator');
+
+describe('Test query validation helper function', () => {
+
+  test('It should return error when no tags are provided', () => {
+    expect(queryValidator().error).toBe('names parameter is required');
+  });
+
+  test('It should return error when invalid sortBy paramter is provided', () => {
+    expect(queryValidator('names', 'author').error).toBe('sortBy parameter is invalid');
+  });
+
+  test('It should return error when invalid direction paramter is provided', () => {
+    expect(queryValidator('names', 'id', 'ascd').error).toBe('direction parameter is invalid');
+  });
+
+  test('It should return null when all parameters are valid', () => {
+    expect(queryValidator('names')).toBe(null);
+    expect(queryValidator('names', 'id', 'asc')).toBe(null);
+  });
+});
