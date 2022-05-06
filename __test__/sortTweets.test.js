6f03ef1f08e1f052ad2dfb983d81e8e85ead6cd0
Roylu7384 <roylu7384@gmail.com>
2022-05-06 11:11:07 -0400

    Implement sortTweets and pass test

--- __test__/sortTweets.test.js
+++ __test__/sortTweets.test.js
@@ -1,16 +1,16 @@
-const sortPosts = require('../helper/sortPosts');
+const sortTweets = require('../helper/sortTweets');
 
-describe('Test posts sorting helper function', () => {
+describe('Test tweets sorting helper function', () => {
 
-  test('It should sort posts based on field and direction', () => {
-    const posts = [{ id: 2 }, { id: 1 }, { id: 3 }];
-    const sortedPosts = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  test('It should sort tweets based on field and direction', () => {
+    const tweets = [{ id: 2 }, { id: 1 }, { id: 3 }];
+    const sortedtweets = [{ id: 3 }, { id: 2 }, { id: 1 }];
 
-    const postsByRate = [{ rate: 2, id: 1 }, { rate: 1, id: 2 }, { rate: 3, id: 3 }];
-    const descSortedPosts = [{ rate: 3, id: 3 }, { rate: 2, id: 1 }, { rate: 1, id: 2 }];
+    const tweetsBylikes = [{ likes: 2, id: 1 }, { likes: 1, id: 2 }, { likes: 3, id: 3 }];
+    const descSortedtweets = [{ likes: 3, id: 3 }, { likes: 2, id: 1 }, { likes: 1, id: 2 }];
 
-    expect(sortPosts(null, null, posts)).toEqual(sortedPosts);
-    expect(sortPosts('id', 'asc', posts)).toEqual(sortedPosts);
-    expect(sortPosts('rate', 'desc', postsByRate)).toEqual(descSortedPosts);
+    expect(sortTweets(null, null, tweets)).toEqual(sortedtweets);
+    expect(sortTweets('id', 'desc', tweets)).toEqual(sortedtweets);
+    expect(sortTweets('likes', null, tweetsBylikes)).toEqual(descSortedtweets);
   });
 });
