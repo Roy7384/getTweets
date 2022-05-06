f04a58808a64abf5aa3a5397a88e1dc83d67daf7
Roylu7384 <roylu7384@gmail.com>
2022-05-06 17:54:13 -0400

    Implement getTweets test with mock axios call and mock helper function

--- __test__/getTweets.test.js
+++ __test__/getTweets.test.js
@@ -1,11 +1,20 @@
 const getTweets = require("../helper/getTweets");
+const getTwitterId = require("../helper/getTwitterId");
 const axios = require("axios");
 
 jest.mock("axios");
 axios.get.mockResolvedValue({ data: { data: [{ id: 1 }] } });
 
-describe("Test getTweets function with mock api call", () => {
+jest.mock("../helper/getTwitterId");
+getTwitterId.mockResolvedValue({ roy: 1 });
 
+describe("Test getTweets function with mock api call", () => {
+  test("It should return mock result", done => {
+    getTweets(["roy"], {}).then(result => {
+      expect(result).toEqual([{ id: 1 }]);
+      done();
+    });
+  });
 
   test("It should not call api if result already in cached result", done => {
     getTweets(['elon'], {'elon': [{id: 1}]}).then(result => {
